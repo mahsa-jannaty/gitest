a556bb646d34b6803076f540851e95f488210f95
mahsa <mahsa.jannaty@gmail.com>
2024-01-18 19:20:14 +0330

    laravel/breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $users = \App\Models\User::all();
     return view('welcome');
 });
 
-Route::get('/test/laravel-pdf', [\App\Http\Controllers\TestController::class, 'laravelPdf']);
-Route::get('/test/activityLog', [\App\Http\Controllers\TestController::class, 'activityLog']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
